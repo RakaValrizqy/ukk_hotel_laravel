4bbbe3be1ff93c6129058a0fca3c247d5a10e5e0
RakaValrizqy <myemail>
2023-02-15 10:59:00 +0700

    Order Migration fix

--- database/migrations/2023_01_18_010229_create_order_table.php
+++ database/migrations/2023_01_18_010229_create_order_table.php
@@ -25,7 +25,7 @@ class CreateOrderTable extends Migration
             $table->integer('room_qty');
             $table->unsignedBigInteger('room_type_id');
             $table->enum('order_status', ['new', 'check_in', 'check_out']);
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
 
             $table->foreign('room_type_id')->references('room_type_id')->on('room_type');
             $table->foreign('user_id')->references('user_id')->on('user');
